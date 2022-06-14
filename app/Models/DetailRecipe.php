c343e746ecdf502a754c2091255a2bcf5dca99f8
Jennifer <jeante05@gmail.com>
2022-06-14 11:15:59 -0500

    update class by process generate data warehouse

--- app/Models/DetailRecipe.php
+++ app/Models/DetailRecipe.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class DetailRecipe extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'recipe_id',
+        'product_id',
+        'quantity',
+    ];
 }
--- app/Repositories/Product/ProductRepositories.php
+++ app/Repositories/Product/ProductRepositories.php
@@ -5,4 +5,6 @@ namespace App\Repositories\Product;
 interface ProductRepositories
 {
     public function productId(string $nameProduct);
+
+    public function listProducts();
 }
